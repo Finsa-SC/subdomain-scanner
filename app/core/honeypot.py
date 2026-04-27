34aa4e39be41148627a84f0668ecd25f21b21e17
Finsa-SC <finsakusumaputra@gmail.com>
2026-04-27 11:47:50 +0700

    add check structural header

--- app/core/honeypot.py
+++ app/core/honeypot.py
@@ -1,4 +1,5 @@
-from models import HONEYPOT_TITLE, HONEYPOT_NAME, HONEYPOT_HASHES, HONEYPOT_SERVERS, OBSOLETE_VERSIONS
+from models import HONEYPOT_TITLE, HONEYPOT_NAME, HONEYPOT_HASHES, HONEYPOT_SERVERS, OBSOLETE_VERSIONS, \
+    SUSPICIOUS_HEADER_ORDERS
 from utils import is_cloudflare
 
 class HoneypotAnalyzer:
@@ -47,10 +48,39 @@ class HoneypotAnalyzer:
 
 
     def check_structural(self):
+        h_hash = self.http.get("body_hash")
+        s_hash = self.https.get("body_hash")
+        h_keys = [k.lower() for k in self.http.get("header_keys", [])]
+        s_keys = [k.lower() for k in self.https.get("header_keys", [])]
+
+        for b_hash in [h_hash, s_hash]:
+            if b_hash in HONEYPOT_HASHES:
+                self.chance += 50
+                self.findings.append(f"Content hash matches known honeypot: {HONEYPOT_HASHES[b_hash]}")
+                break
+
+        for keys in [h_keys, s_keys]:
+            if not keys: return
+            for trap_order in SUSPICIOUS_HEADER_ORDERS:
+                if all(item in keys for item in trap_order):
+                    indices = [keys.index(item) for item in trap_order]
+                    if indices == sorted(indices):
+                        self.chance += 20
+                        self.findings.append("Suspicious HTTP header ordering detected")
+                        break
+
+        h_size = self.http.get("size", 0)
+        s_size = self.https.get("size", 0)
+        if h_size == s_size and h_size > 0:
+            self.chance += 10
+            self.findings.append("Identical response size on both protocols")
 
 
     def check_behavioral(self):
         ...
 
     def run_all(self):
-        return self.chance, self.findings
+        self.check_static()
+        self.check_structural()
+
+        return min(self.chance, 100), self.findings
