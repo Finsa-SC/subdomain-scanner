da5accec6b9a471743b1e7aab4c4b9b1089feb5b
Finsa-SC <finsakusumaputra@gmail.com>
2026-05-15 06:57:32 +0700

    feat: add symlink to tail with latest logs

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -13,6 +13,14 @@ def get_logger(name: str) -> logging.Logger:
 
     date_str = datetime.now().strftime('%Y-%m-%d')
     log_file = log_dir / f"scan_{date_str}.log"
+    log_latest = log_dir / 'latest.log'
+
+    if log_latest.exists() or log_latest.is_symlink():
+        log_latest.unlink()
+    try:
+        log_latest.symlink_to(log_file.name)
+    except:
+        pass
 
     formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
     handler = logging.FileHandler(log_file)
