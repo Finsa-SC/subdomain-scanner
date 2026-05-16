4ccc9972e3896dcb2275319d4de765e46a846729
Finsa-SC <finsakusumaputra@gmail.com>
2026-05-16 19:40:58 +0700

    fix: add io stream for crtsh data consumtion

--- app/sources/crtsh.py
+++ app/sources/crtsh.py
@@ -1,4 +1,4 @@
-import ijson
+import ijson, io
 from utils import get_logger
 
 def fetch_crtsh(domain: str):
@@ -6,13 +6,14 @@ def fetch_crtsh(domain: str):
     log = get_logger("crtsh")
     url = f"https://crt.sh/?q={domain}&output=json"
     try:
-        res = send_request(method="GET", url=url, timeout=10)
+        res = send_request(method="GET", url=url, timeout=15)
         if not res:
             log.error("Failed to fetch data from crt.sh (Connection or Proxy Error).")
             return
 
         if res.status_code == 200:
-            for entry in ijson.items(res.raw, "item"):
+            f_stream = io.BytesIO(res.content)
+            for entry in ijson.items(f_stream, "item"):
                 name = entry['name_value'].lower()
                 for n in name.split("\n"):
                     clean = n.replace("*.", "").strip()
